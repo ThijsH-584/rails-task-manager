c2401beed73498b6d1a144572e2cb59b5f3ca341
Thijs Hakkenberg <thijshakkenberg@hotmail.com>
2020-08-18 15:16:03 +0200

    Proto final version

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,52 @@
 class TasksController < ApplicationController
+	# before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+
 	def index
 		@tasks = Task.all
 	end
 
-	def list
-		@tasks = Task.find(params[:id])
+	def show
+		@task = Task.find(params[:id])
 	end
+
+	#create methods
+	def new
+		@task = Task.new
+	end
+
+	def create
+		@task = Task.create(params[:task].permit(:title, :details))
+		redirect_to tasks_path
+	end
+
+	# update methods
+	def edit
+		@task = Task.find(params[:id])
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update(params[:task].permit(:title, :details, :completed))
+		redirect_to tasks_path
+	end
+
+	# destroy method
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		redirect_to tasks_path
+	end
+
+	# private
+	# def strong_params
+	# 	params[:task].permit(:title, :details)
+	# end
+
+	# private
+	# def find_task
+	# 	@task = Task.find(params[:id])
+	# end
 end
+
+
